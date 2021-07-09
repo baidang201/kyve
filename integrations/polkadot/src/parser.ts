d65fb293e9bd25d899213515a3ae7c0ef40abe18
Li Smith <jinrishouji@sina.com>
2021-07-09 21:09:04 +0800

    update custom polkadot parser

--- integrations/polkadot/src/parser.ts
+++ integrations/polkadot/src/parser.ts
@@ -1,47 +1,19 @@
 export const parseBlockByNumber = async (
-  blockNumber: number,
-  api: any
-): Promise<{ block: any; tags: any }> => {
-  const hash = await api.rpc.chain.getBlockHash(blockNumber);
-  let block = (await api.rpc.chain.getBlock(hash)).block;
-  const timestamp = (await api.query.timestamp.now.at(hash)).toNumber();
-
-  const extrinsics: any[] = [];
-  const rawEvents = await api.query.system.events.at(hash);
-
-  block.extrinsics.forEach((ex: any, index: number) => {
-    const tx = ex.toHuman();
-    const events: any = [];
-
-    const filteredEvents = rawEvents.filter(
-      ({ phase }: any) =>
-        phase.isApplyExtrinsic && phase.asApplyExtrinsic.eq(index)
-    );
-    filteredEvents.forEach((event: any) => {
-      const res = event.toHuman().event;
-      events.push(res);
-    });
-
-    extrinsics.push({
-      hash: ex.hash.toHuman(),
-      ...tx,
-      events,
-    });
-  });
-
-  block = {
-    hash: hash.toHex(),
-    number: blockNumber,
-    timestamp,
-    parentHash: block.toHuman().header.parentHash,
-    extrinsics,
-    digest: block.toHuman().header.digest,
+    blockNumber: number,
+    api: any,
+    client: any
+  ): Promise<{ block: any; tags: any }> => {
+    const hash = await api.rpc.chain.getBlockHash(blockNumber);
+    
+    const result = await client.request({ method: "chain_getBlock", params: [hash.toHex()] });
+    let block = JSON.stringify(result)
+  
+    const tags = [
+      { name: "ChaidId", value: 9999 },
+      { name: "Height", value: blockNumber },
+      { name: "Block", value: hash.toHex() },
+    ];
+  
+    return { block, tags };
   };
-
-  const tags = [
-    { name: "Height", value: block.number },
-    { name: "Block", value: block.hash },
-  ];
-
-  return { block, tags };
-};
+  
